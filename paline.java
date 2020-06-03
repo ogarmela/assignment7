b32188162485090d8d853f4b3bb9862985458870
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-06-02 18:40:52 -0700

    Add files via upload

---
+++ paline.java
@@ -0,0 +1,30 @@
+import java.util.Queue;
+import java.util.Stack;
+
+public class paline {
+	public boolean isPalindrome(Queue<Integer> q) {
+	    Stack<Integer> s = new Stack<Integer>();
+	    int size = q.size();
+	    boolean palindrome = true;
+	                
+	    for(int i = 0; i < size; i++) {
+	        int n = q.remove();
+	        q.add(n);
+	        s.push(n);
+	    }
+	                                                    
+	    for(int i = 0; i < size; i++) {
+	        int n1 = q.remove();
+	        int n2 = s.pop();
+	        
+	        if(n1 != n2)
+	            palindrome = false;
+	            
+	        q.add(n1);
+	    }
+	    
+	    return palindrome;
+	}
+
+
+}
---
+++ shutter.java
@@ -0,0 +1,22 @@
+
+public class shutter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String myText = "";
+
+	     for (int i = 0; i < a.length(); i++) {
+
+	           myText = myText + a.charAt(i) + a.charAt(i);
+
+	      }
+
+	    return myText;
+
+	 }
+
+
+
+	}
+
+
